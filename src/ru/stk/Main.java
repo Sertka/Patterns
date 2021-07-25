e02be5ce446807f1787a657262a461afacd9d07b
serge <stk77@yandex.ru>
2021-07-25 16:55:06 +0300

    HW03

--- src/ru/stk/Main.java
+++ src/ru/stk/Main.java
@@ -1,8 +1,16 @@
 package ru.stk;
 
+import com.sun.tools.corba.se.idl.constExpr.Or;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    OrderFactory orderFactory = new OrderFactoryImpl();
+        Order o1 = orderFactory.createOrder();
+        Order o2 = orderFactory.createOrder();
+
+        System.out.println(o1.getId());
+        System.out.println(o2.getId());
+
     }
 }
---
+++ src/ru/stk/Order.java
@@ -0,0 +1,111 @@
+package ru.stk;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+/**
+ * Order contains list of products for each purchasing
+ */
+
+public class Order {
+
+    private Long id;
+
+    private Long userId;
+
+    private Integer status;
+
+    private BigDecimal price;
+
+    private LocalDateTime deliveryDate;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    private String phone;
+
+    private String address;
+
+    private String details;
+
+    public Order(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDateTime deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+}

34ec1158ccd5a7488ae7dacfaafffab3dcfa6e87
serge <stk77@yandex.ru>
2021-08-05 17:36:10 +0300

    HW05

--- src/ru/stk/Main.java
+++ src/ru/stk/Main.java
@@ -5,18 +5,32 @@ import com.sun.tools.corba.se.idl.constExpr.Or;
 public class Main {
 
     public static void main(String[] args) {
-        // Создаем объект-описание
-        OrderDescription od = new OrderDescription();
-        od.setOrderId(1L);
-        od.setUserName("Вася");
+        // Создаем заказ
+        Order o = new Order(1L);
 
-        //Создаем адаптер
-        OrderAdapter oa = new OrderAdapter(od);
-        System.out.println(oa.getUserId());
+        //Создаем сервис
+        OrderService oService = new OrderService();
 
-        //Запускаем обновление пользователя в заказе
-        OrderService os = new OrderService();
-        Order o = os.updateOrderUser(oa, 5L);
-        System.out.println(o.getUserId());
+        //Создаем подписчика 1
+        OrderSubscriber os1 = new OrderSubscriber() {
+            @Override
+            public void inform(OrderService os, Object o, String propertyChangedName) {
+                System.out.println("S1: Новое значение свойства " + propertyChangedName + " - " + o);
+            }
+        };
+
+        //Создаем подписчика 1
+        OrderSubscriber os2 = new OrderSubscriber() {
+            @Override
+            public void inform(OrderService os, Object o, String propertyChangedName) {
+                System.out.println("S2: Новое значение свойства " + propertyChangedName + " - " + o);
+            }
+        };
+
+        oService.subscribe(os1);
+        oService.subscribe(os2);
+
+        oService.updateOrderStatus(o, 1);
+        oService.updateOrderUser(o, 12L);
     }
 }
--- src/ru/stk/OrderService.java
+++ src/ru/stk/OrderService.java
@@ -1,10 +1,38 @@
 package ru.stk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderService {
+
+    //реализуем паттерн Subscriber
+    private List<OrderSubscriber> osList = new ArrayList<>();
+
+    public void subscribe(OrderSubscriber os){
+        osList.add(os);
+    }
+
+    public void unsubscribe(OrderSubscriber os){
+        osList.remove(os);
+    }
 
     public Order updateOrderUser(Order o, Long newUserId){
         System.out.println("New UserID update in DB:" + newUserId);
         o.setUserId(newUserId);
+        //информируем подписчиков
+        for (OrderSubscriber os: osList){
+            os.inform(this, newUserId, "UserId");
+        }
+        return o;
+    }
+
+    public Order updateOrderStatus(Order o, Integer newStatus){
+        System.out.println("New Order Status updated in DB:" + newStatus);
+        o.setStatus(newStatus);
+            for (OrderSubscriber os: osList){
+                //информируем подписчиков
+                os.inform(this, newStatus, "Status");
+            }
         return o;
     }
 }
